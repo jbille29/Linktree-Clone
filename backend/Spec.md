65115d62f5a9dcde721469943d170b93124dbdfe
Joseph Bille <jbille29@gmail.com>
2023-01-17 19:02:58 -0600

    add backend and frontend folders

---
+++ backend/Spec.md
@@ -0,0 +1,4 @@
+1. [ ] Need to be able to create user accounts
+2. [ ] Users will need login functionality
+3. [ ] Users will need to be able to create categories
+4. [ ] Users will need to be able to create posts in their categories
---
+++ backend/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const { logEvents } = require('./middleware/logger')
+const PORT = process.env.PORT || 3500
+const app = express()
+
+connectDB()
+
+app.use(express.json())
+
+app.use(logger)
+
+app.use('/users', require('./routes/userRoutes'))
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+ })
+
+ mongoose.connection.on('error',  err => { 
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+ })
